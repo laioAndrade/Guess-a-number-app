2d36728bf448c2baa110da0e936548d6a7521233
Laio Andrade <laioand98@gmail.com>
2020-11-12 16:37:31 -0300

    add newgame button

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>The game is over</Text>
+      <Text>Number of Rounds: {props.roundNumber}</Text>
+      <Text>Number was: {props.userNumber}</Text>
+      <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   );
 };
